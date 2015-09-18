92e69540ee5a51cfe7ca469873336bbbe1379c3b
yasinaaa <yahootadaa@gmail.com>
2015-09-18 19:22:10 +0300

    delete rubbish

--- IdeaProjects/spring-mvc-4/src/main/java/springmvc/service/UserManager.java
+++
@@ -1,7 +0,0 @@
-package springmvc.service;
-
-/**
- * Created by yasina on 07.05.15.
- */
-public class UserManager {
-}
--- IdeaProjects/spring-mvc-4/src/main/java/springmvc/web/UserController.java
+++
@@ -1,7 +0,0 @@
-package springmvc.web;
-
-/**
- * Created by yasina on 07.05.15.
- */
-public class UserController {
-}
